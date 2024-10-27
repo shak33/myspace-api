715c4ef88ade08852436ce63040bd1828861f5b0
shak33 <maciej@poppek.pl>
2024-10-27 13:58:41 +0100

    Written integration test for /auth/login route, created fixture loginUser

--- __tests__/routes/auth/authRoutes.integration.test.ts
+++ __tests__/routes/auth/authRoutes.integration.test.ts
@@ -3,8 +3,11 @@ import { expect } from 'chai';
 import { server } from 'globalSetup.mocha';
 
 import { prisma } from '@/db';
-import registerUserData from '@/tests/fixtures/user/registerUser.json';
 import { API_ROUTE_AUTH, authRoutes } from '@/routes/routes';
+import { createUserFactory } from '@/tests/factories/user/createUser.factory';
+
+import registerUserData from '@/tests/fixtures/user/registerUser.json';
+import loginUserData from '@/tests/fixtures/user/loginUser.json';
 
 describe('ROUTE /api/v1/auth', () => {
   describe('/register', () => {
@@ -40,6 +43,35 @@ describe('ROUTE /api/v1/auth', () => {
         .send(registerUserData);
 
       expect(res.status).to.equal(409);
+      expect(res.body).to.have.property('success').equal(false);
+    });
+  });
+
+  describe('/login', () => {
+    before(async () => {
+      await prisma.profile.deleteMany();
+      await prisma.user.deleteMany({});
+      await createUserFactory();
+    });
+
+    it('should return 200 with token', async () => {
+      const res = await request(server)
+        .post(`${API_ROUTE_AUTH}${authRoutes.login}`)
+        .send(loginUserData);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.data).to.have.property('token');
+    });
+
+    it('should return 401 with success set to false', async () => {
+      const res = await request(server)
+        .post(`${API_ROUTE_AUTH}${authRoutes.login}`)
+        .send({
+          ...loginUserData,
+          password: 'passsword',
+        });
+
+      expect(res.status).to.equal(401);
       expect(res.body).to.have.property('success').equal(false);
     });
   });
