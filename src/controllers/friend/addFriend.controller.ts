c2ace593a71f09ada5f042805233ae53d166b25f
shak33 <maciej@poppek.pl>
2024-10-30 12:01:05 +0100

    Created logic for addFriend controller, created mail notification about new friend request

--- src/controllers/friend/addFriend.controller.ts
+++ src/controllers/friend/addFriend.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { prisma } from '@/db';
+import { sendMail } from '@/utils/sendMail.util';
 
 export const addFriendController = async (
   req: Request,
@@ -6,6 +8,80 @@ export const addFriendController = async (
   next: NextFunction
 ) => {
   try {
+    // @ts-ignore
+    const loggedInUser = req.user;
+    const { userId } = req.params;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        userId,
+      },
+      include: {
+        profile: true,
+        sentFriendRequests: {
+          where: { userId2: loggedInUser.id },
+        },
+        receivedFriendRequests: {
+          where: { userId1: loggedInUser.id },
+        },
+        friendsAsUser1: {
+          where: { userId2: loggedInUser.id },
+        },
+        friendsAsUser2: {
+          where: { userId1: loggedInUser.id },
+        },
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });
+    }
+
+    if (user.friendsAsUser1.length > 0 || user.friendsAsUser2.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: 'You are already friends with this user',
+      });
+    }
+
+    if (user.receivedFriendRequests.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: 'You already have a pending friend request from this user',
+      });
+    }
+
+    if (user.sentFriendRequests.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: 'You have already sent a friend request to this user',
+      });
+    }
+
+    const sendFriendRequest = await prisma.friendRequest.create({
+      data: {
+        userId1: loggedInUser.id,
+        userId2: user.id,
+      },
+    });
+
+    const sendFriendRequestMail = await sendMail({
+      email: user.email,
+      subject: `MySpace.com - New friend request from ${loggedInUser.firstName} ${loggedInUser.lastName}`,
+      template: '/friend/friend-request.ejs',
+      data: {
+        requestingUser: `${loggedInUser.firstName} ${loggedInUser.lastName}`,
+        receivingUser: `${user.profile?.firstName} ${user.profile?.lastName}`,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: 'Friend request has been sent',
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
